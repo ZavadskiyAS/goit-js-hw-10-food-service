1c4a5dbf49419bd005108731c7ecc8205e824e67
ZavadskiyAS <Zavadskiyweb@gmail.com>
2020-05-22 16:38:53 +0300

    finish

--- src/changeTheme.js
+++ src/changeTheme.js
@@ -1,31 +1,31 @@
-// import { loadStorage, saveStorage } from './storage';
-// const refs = {
-//   themeCheckbox: document.querySelector('.js-switch-input'),
-//   body: document.body,
-// };
+import { loadStorage, saveStorage } from './storage';
+const refs = {
+  themeCheckbox: document.querySelector('.js-switch-input'),
+  body: document.body,
+};
 
-// const Theme = {
-//   LIGHT: 'light-theme',
-//   DARK: 'dark-theme',
-// };
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
 
-// const darkTheme = loadStorage('Theme');
+const darkTheme = loadStorage('Theme');
 
-// if (darkTheme === 'DARK') {
-//   refs.themeCheckbox.checked = true;
-//   refs.body.classList.add(Theme.DARK);
-// }
+if (darkTheme === 'DARK') {
+  refs.themeCheckbox.checked = true;
+  refs.body.classList.add(Theme.DARK);
+}
 
-// refs.themeCheckbox.addEventListener('change', hendleChange);
+refs.themeCheckbox.addEventListener('change', hendleChange);
 
-// function hendleChange(e) {
-//   if (e.currentTarget.checked === true) {
-//     refs.body.classList.remove(Theme.LIGHT);
-//     refs.body.classList.add(Theme.DARK);
-//     saveStorage('Theme', 'DARK');
-//   } else if (e.currentTarget.checked === false) {
-//     refs.body.classList.remove(Theme.DARK);
-//     refs.body.classList.add(Theme.LIGHT);
-//     saveStorage('Theme', 'LIGHT');
-//   }
-// }
+function hendleChange(e) {
+  if (e.currentTarget.checked === true) {
+    refs.body.classList.remove(Theme.LIGHT);
+    refs.body.classList.add(Theme.DARK);
+    saveStorage('Theme', 'DARK');
+  } else if (e.currentTarget.checked === false) {
+    refs.body.classList.remove(Theme.DARK);
+    refs.body.classList.add(Theme.LIGHT);
+    saveStorage('Theme', 'LIGHT');
+  }
+}
--- src/storage.js
+++ src/storage.js
@@ -1,18 +1,18 @@
-// export const loadStorage = key => {
-//     try {
-//       const checkingState = localStorage.getItem(key);
+export const loadStorage = key => {
+    try {
+      const checkingState = localStorage.getItem(key);
   
-//       return checkingState === null ? undefined : JSON.parse(checkingState);
-//     } catch (err) {
-//       console.error('Get state error: ', err);
-//     }
-//   };
+      return checkingState === null ? undefined : JSON.parse(checkingState);
+    } catch (err) {
+      console.error('Get state error: ', err);
+    }
+  };
   
-//   export const saveStorage = (key, value) => {
-//     try {
-//       const checkingState = JSON.stringify(value);
-//       localStorage.setItem(key, checkingState);
-//     } catch (err) {
-//       console.error('Set state error: ', err);
-//     }
-//   };
+  export const saveStorage = (key, value) => {
+    try {
+      const checkingState = JSON.stringify(value);
+      localStorage.setItem(key, checkingState);
+    } catch (err) {
+      console.error('Set state error: ', err);
+    }
+  };
